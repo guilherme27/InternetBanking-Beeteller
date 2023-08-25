57bbd6ea8a1ad1fbc36d57567e73f5e675f617b1
Guilherme P <guilherme27.gp@gmail.com>
2023-08-25 19:17:09 -0300

    feat(index): added index for easier component access

--- src/main/components/atoms/index.ts
+++ src/main/components/atoms/index.ts
@@ -1,5 +1,6 @@
-export { default as CircleButton } from './CircleButton/circle-button.atom';
 export { default as Button } from './Button/button.atom';
-export { default as MenuItem } from './MenuItem/menu-item.atom';
+export { default as CircleButton } from './CircleButton/circle-button.atom';
 export { default as Icon } from './Icon/icon.atom';
+export { default as Image } from './Image/image.atom';
+export { default as Avatar } from './Avatar/avatar.atom';
 export { default as Typography } from './Typography/typography.atom';
