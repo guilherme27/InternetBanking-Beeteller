0ca9bd7d1525f0cfb9542543bca2a3df1f05b481
Guilherme P <guilherme27.gp@gmail.com>
2023-08-25 19:30:40 -0300

    chore(styles): updated global styles

--- src/common/styles/global.styles.ts
+++ src/common/styles/global.styles.ts
@@ -1,16 +1,26 @@
 import { createGlobalStyle, DefaultTheme } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle<{ theme?: DefaultTheme }>`
-    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@100;200;300;400;500;600;700;800&display=swap');
     
-    head, body{
+    html, body {
         font-family: 'Open Sans', system-ui, -apple-system, 'Helvetica Neue', sans-serif;
         box-sizing: border-box;
         font-size: 16px;
+        font-weight: 500;
         padding: 0;
         margin: 0;
         border: 0;
         vertical-align: baseline;
         overflow: hidden;
+
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-rendering: optimizeLegibility;
+    }
+
+    button { 
+        font-family: 'Open Sans', system-ui, -apple-system, 'Helvetica Neue', sans-serif;
+        font-weight: 500;
     }
 `;
