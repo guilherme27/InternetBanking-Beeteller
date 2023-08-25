403388ce795ced3aa90db411186086594fb5204f
Guilherme P <guilherme27.gp@gmail.com>
2023-08-25 19:09:49 -0300

    feat(atom): created "icon" component atom feat(helper): added helper for "icon" component

--- src/main/components/atoms/Icon/icon.styles.ts
+++ src/main/components/atoms/Icon/icon.styles.ts
@@ -2,13 +2,13 @@ import { styled } from 'styled-components';
 
 import { IconSize } from './icon.atom';
 
-const sizes = {
-  sm: '1rem',
-  md: '1.5rem',
-  lg: '2rem',
-  xl: '2.5rem',
-};
-
 export const Icon = styled.i<{ size: IconSize }>`
-  font-size: ${(props) => sizes[props.size]};
+  ${({ size, theme }) => `font-size: ${theme.fonts.size[size]}`};
+  ${({ color, theme }) => color === 'base' && `color: ${theme.colors.primary.base}`}
+  ${({ color, theme }) => color === 'accent' && `color: ${theme.colors.primary.accent}`}
+  ${({ color, theme }) => color === 'white' && `color: ${theme.colors.primary.white}`}
+  ${({ color, theme }) => color === 'black' && `color: ${theme.colors.neutral.black}`}
+  ${({ color, theme }) => color === 'dark' && `color: ${theme.colors.neutral.dark}`}
+  ${({ color, theme }) => color === 'success' && `color: ${theme.colors.status.success}`}
+  ${({ color, theme }) => color === 'danger' && `color: ${theme.colors.status.danger}`}
 `;
